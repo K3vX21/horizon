6da43f02fe2373e43a8bd08258cbc0c92b124e35
K3vX21 <54183095+K3vX21@users.noreply.github.com>
2022-07-19 20:25:13 -0500

    bad js image upload

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// weather at location
 const searchElement = document.querySelector('[data-city-search]')
 const searchBox = new google.maps.places.SearchBox(searchElement)
 searchBox.addListener('places_changed', () => {
@@ -80,3 +81,58 @@ function setWeatherData(data, location, sunsetProb){
     temperatureElement.textContent = data.temp + "℉"
 
 }
+
+// image
+const loadElement = document.querySelector('#btnLoad')
+loadElement.addEventListener('click', analyzeImage)
+
+function analyzeImage() {
+    const file = document.querySelector('#imgFile').files[0]
+    console.log(file)
+    const reader = new FileReader();
+    reader.readAsDataURL(file)
+    console.log("1")
+    reader.onloadedend = () => {
+        console.log("2")
+        //     HTMLImageElement
+        // These are images created using the Image() constructor, as well as any <img> element.
+        const img = new Image();
+        img.height = 1000
+        img.width = 1000
+        img.src = file.result
+    }
+    const ctx = document.querySelector('canvas').getContext('2d')
+    // file.onload = ctx.drawImage(img, 0, 0)\
+}
+
+
+
+
+// Get a reference to an HTMLImageElement object or to another
+// canvas element as a source. It is also possible to use images
+// by providing a URL.
+
+// Draw the image on the canvas using the drawImage() function.
+
+
+
+let imgInput = document.getElementById('imageInput');
+imgInput.addEventListener('change', function (e) {
+    if (e.target.files) {
+        let imageFile = e.target.files[0]; //here we get the image file
+        var reader = new FileReader();
+        reader.readAsDataURL(imageFile);
+        reader.onloadend = function (e) {
+            var myImage = new Image(); // Creates image object
+            myImage.src = e.target.result; // Assigns converted image to image object
+            myImage.onload = function (ev) {
+                var myCanvas = document.getElementById("myCanvas"); // Creates a canvas object
+                var myContext = myCanvas.getContext("2d"); // Creates a contect object
+                myCanvas.width = myImage.width; // Assigns image's width to canvas
+                myCanvas.height = myImage.height; // Assigns image's height to canvas
+                myContext.drawImage(myImage, 0, 0); // Draws the image on canvas
+                let imgData = myCanvas.toDataURL("image/jpeg", 0.75); // Assigns image base64 string in jpeg format to a variable
+            }
+        }
+    }
+})
